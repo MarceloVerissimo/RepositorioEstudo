8aa558965d376603c282faa25c09bf63da65b27a
MarceloVerissimo <verissimomarcelo95@gmail.com>
2024-08-20 20:24:15 -0300

    Estudoos

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("testando")
+var a = 10;
+var b = 20;
+var total = a + b;
+console.log(total);
+
+var nomePessoa = "Marcelo";
+var NOMEPESSOA = "MARCELO"
+console.log(nomePessoa)
+
+var var1 = 1,
+    var2 = 2,
+    var3 = 3;
+
+    console.log(var3)
+
+let book = 10;
+book = 20;
+console.log(book);
+
+const valor1 = 100;
+valor1 = 200; //CONSTANTE É UM VALOR QUE NÃO PODE SER ALTERADO
+console.log(valor1)
--- script2.js
+++ script2.js
@@ -1,3 +1,8 @@
-let rlSync = require('readline-sync');
-let nome = rlSync.question("Qual o seu nome?\n")
-console.log(`Olá, ${nome}`)
+function perguntarNome() {
+    let rlSync = require('readline-sync');
+    let nome = rlSync.question("Qual o seu nome?\n");
+    console.log(`Olá, ${nome}`);
+}
+
+// Chame a função
+perguntarNome();
